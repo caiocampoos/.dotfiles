a23d0c66c0eb8b8510c5cfb323f4109716a1f27d
caiocampos <caiocampoosbr@gmail.com>
2022-08-04 15:15:57 -0300

    lua config

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
-# Install Nix
-
+# install nix
 curl -L https://nixos.org/nix/install | sh
 
-# Source Nix
+# source nix
 . ~/.nix-profile/etc/profile.d/nix.sh
 
-# install packages 
- 
+# install packages
 nix-env -iA \
 	nixpkgs.zsh \
 	nixpkgs.antibody \
@@ -18,10 +16,9 @@ nix-env -iA \
 	nixpkgs.fzf \
 	nixpkgs.ripgrep \
 	nixpkgs.bat \
-	nixpkgs.direnv \
-	nixpkgs.gcc \
 	nixpkgs.gnumake \
-    nixpkgs.go \
+	nixpkgs.gcc \
+	nixpkgs.direnv
 
 # stow dotfiles
 stow git
@@ -29,21 +26,14 @@ stow nvim
 stow tmux
 stow zsh
 
-# set up zsh as login shell
-
+# add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 
-#source zsh as defautl shells
-
+# use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-#install neovim plugins 
-
-nvim --headless +PlugInstall +qall
-
-# bundle zsh plugins
-
+# bundle zsh plugins 
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
-
-
+# install neovim plugins
+nvim --headless +PlugInstall +qall
