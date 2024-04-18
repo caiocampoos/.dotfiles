5484c439af2f476cd6499b27d20979a1ac9da87e
caiocamopoos <caiocampoosbr@gmail.com>
2024-04-18 09:19:12 -0300

    fix(nvim): none-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,15 +3,13 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
-      debug = false,
       sources = {
-        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
       },
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end
+  end,
 }
